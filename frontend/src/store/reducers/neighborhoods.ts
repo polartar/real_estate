d791381f6f4ccb676ae8ce5fa40e6d481b67ec61
polartar <polaris.center@outlook.com>
2019-10-17 21:42:48 -0700

    fill in neighborhood labels for demo

--- frontend/src/store/reducers/neighborhoods.ts
+++ frontend/src/store/reducers/neighborhoods.ts
@@ -28,190 +28,304 @@ const getInitialState = () => {
         },
         {
           id: 4,
-          name: 'Battery Park City',
-          slug: 'battery-park-city',
+          name: 'Fulton/Seaport',
+          slug: 'fulton-seaport',
           image: '/assets/images/neighborhoods/battery-park-city.jpg'
         },
         {
           id: 5,
-          name: 'Tribeca',
-          slug: 'tribeca',
+          name: 'Civic/Center',
+          slug: 'civic-center',
           image: '/assets/images/neighborhoods/tribeca.jpg'
         },
         {
           id: 6,
-          name: 'Financial District',
-          slug: 'financial-district',
+          name: 'Chinatown',
+          slug: 'chinatown',
           image: '/assets/images/financial-district.jpg'
         },
         {
           id: 7,
-          name: 'Battery Park City',
-          slug: 'battery-park-city',
+          name: 'Two Bridges',
+          slug: 'two-bridges',
           image: '/assets/images/neighborhoods/battery-park-city.jpg'
         },
         {
           id: 8,
-          name: 'Tribeca',
-          slug: 'tribeca',
+          name: 'Lower East Side',
+          slug: 'lower-east-side',
           image: '/assets/images/neighborhoods/tribeca.jpg'
         },
         {
           id: 9,
-          name: 'Financial District',
-          slug: 'financial-district',
+          name: 'Little Italy',
+          slug: 'little-italy',
           image: '/assets/images/financial-district.jpg'
         },
         {
           id: 10,
-          name: 'Battery Park City',
-          slug: 'battery-park-city',
+          name: 'Nolita',
+          slug: 'nolita',
           image: '/assets/images/neighborhoods/battery-park-city.jpg'
         },
         {
           id: 11,
-          name: 'Tribeca',
-          slug: 'tribeca',
+          name: 'Soho',
+          slug: 'soho',
           image: '/assets/images/neighborhoods/tribeca.jpg'
         },
         {
           id: 12,
-          name: 'Financial District',
-          slug: 'financial-district',
+          name: 'Noho',
+          slug: 'noho',
           image: '/assets/images/financial-district.jpg'
         },
         {
           id: 13,
-          name: 'Battery Park City',
-          slug: 'battery-park-city',
+          name: 'Greenwich Village',
+          slug: 'greenwich-village',
           image: '/assets/images/neighborhoods/battery-park-city.jpg'
         },
         {
           id: 14,
-          name: 'Tribeca',
-          slug: 'tribeca',
+          name: 'East Village',
+          slug: 'east-village',
           image: '/assets/images/neighborhoods/tribeca.jpg'
         },
         {
           id: 15,
-          name: 'Financial District',
-          slug: 'financial-district',
+          name: 'West Village',
+          slug: 'west-village',
           image: '/assets/images/financial-district.jpg'
         },
         {
           id: 16,
-          name: 'Battery Park City',
-          slug: 'battery-park-city',
+          name: 'Stuyvesant Town',
+          slug: 'stuyvesant-town',
           image: '/assets/images/neighborhoods/battery-park-city.jpg'
         },
         {
           id: 17,
-          name: 'Tribeca',
-          slug: 'tribeca',
+          name: 'Gramercy Park',
+          slug: 'gramercy-park',
           image: '/assets/images/neighborhoods/tribeca.jpg'
         },
         {
           id: 18,
-          name: 'Financial District',
-          slug: 'financial-district',
+          name: 'Chelsea',
+          slug: 'chelsea',
           image: '/assets/images/financial-district.jpg'
         },
         {
           id: 19,
-          name: 'Battery Park City',
-          slug: 'battery-park-city',
+          name: 'West Chelsea',
+          slug: 'west-chelsea',
           image: '/assets/images/neighborhoods/battery-park-city.jpg'
         },
         {
           id: 20,
-          name: 'Tribeca',
-          slug: 'tribeca',
+          name: 'Flatiron',
+          slug: 'flatiron',
           image: '/assets/images/neighborhoods/tribeca.jpg'
-        },
-        {
-          id: 21,
-          name: 'Financial District',
-          slug: 'financial-district',
-          image: '/assets/images/financial-district.jpg'
-        },
+        }
       ],
       'Midtown': [
         {
           id: 22,
-          name: 'Battery Park City',
-          slug: 'battery-park-city',
+          name: 'Midtown East',
+          slug: 'midtown-east',
           image: '/assets/images/neighborhoods/battery-park-city.jpg'
         },
         {
           id: 23,
-          name: 'Tribeca',
-          slug: 'tribeca',
+          name: 'Kips Bay',
+          slug: 'kips-bay',
           image: '/assets/images/neighborhoods/tribeca.jpg'
         },
         {
           id: 24,
-          name: 'Financial District',
-          slug: 'financial-district',
+          name: 'Murray Hill',
+          slug: 'murray-hill',
+          image: '/assets/images/financial-district.jpg'
+        },
+        {
+          id: 25,
+          name: 'Turtle Bay',
+          slug: 'turtle-bay',
+          image: '/assets/images/neighborhoods/battery-park-city.jpg'
+        },
+        {
+          id: 26,
+          name: 'Beekman',
+          slug: 'beekman',
+          image: '/assets/images/neighborhoods/tribeca.jpg'
+        },
+        {
+          id: 27,
+          name: 'Sutton Place',
+          slug: 'sutton-place',
+          image: '/assets/images/financial-district.jpg'
+        },
+        {
+          id: 28,
+          name: 'Midtown',
+          slug: 'midtown',
+          image: '/assets/images/financial-district.jpg'
+        },
+        {
+          id: 29,
+          name: 'Central Park South',
+          slug: 'central-park-south',
+          image: '/assets/images/financial-district.jpg'
+        },
+        {
+          id: 30,
+          name: 'Midtown South',
+          slug: 'midtown-south',
+          image: '/assets/images/financial-district.jpg'
+        },
+        {
+          id: 31,
+          name: 'Midtown West',
+          slug: 'midtown-west',
+          image: '/assets/images/financial-district.jpg'
+        },
+        {
+          id: 32,
+          name: 'Roosevelt Island',
+          slug: 'roosevelt-island',
           image: '/assets/images/financial-district.jpg'
         },
       ],
       'Upper East': [
         {
-          id: 25,
-          name: 'Battery Park City',
-          slug: 'battery-park-city',
+          id: 33,
+          name: 'Upper East Side',
+          slug: 'upper-east-side',
           image: '/assets/images/neighborhoods/battery-park-city.jpg'
         },
         {
-          id: 26,
-          name: 'Tribeca',
-          slug: 'tribeca',
+          id: 34,
+          name: 'Lenox Hill',
+          slug: 'lenox-hill',
           image: '/assets/images/neighborhoods/tribeca.jpg'
         },
         {
-          id: 27,
-          name: 'Financial District',
-          slug: 'financial-district',
+          id: 35,
+          name: 'Yorkville',
+          slug: 'yorkville',
           image: '/assets/images/financial-district.jpg'
         },
+        {
+          id: 36,
+          name: 'Carnegie Hill',
+          slug: 'carnegie-hill',
+          image: '/assets/images/financial-district.jpg'
+        },
+        {
+          id: 37,
+          name: 'Upper Carnegie Hill',
+          slug: 'upper-carnegie-hill',
+          image: '/assets/images/financial-district.jpg'
+        }
       ],
       'Upper West': [
         {
-          id: 28,
-          name: 'Battery Park City',
-          slug: 'battery-park-city',
+          id: 38,
+          name: 'Upper West Side',
+          slug: 'upper-west-side',
           image: '/assets/images/neighborhoods/battery-park-city.jpg'
         },
         {
-          id: 29,
-          name: 'Tribeca',
-          slug: 'tribeca',
+          id: 39,
+          name: 'Lincoln Square',
+          slug: 'lincoln-square',
           image: '/assets/images/neighborhoods/tribeca.jpg'
         },
         {
-          id: 30,
-          name: 'Financial District',
-          slug: 'financial-district',
+          id: 40,
+          name: 'Manhattan Valley',
+          slug: 'manhattan-valley',
           image: '/assets/images/financial-district.jpg'
         },
+        {
+          id: 41,
+          name: 'Morningside Heights',
+          slug: 'morningside-heights',
+          image: '/assets/images/morningside-heights.jpg'
+        }
       ],
       'Upper Manhattan': [
         {
-          id: 31,
-          name: 'Battery Park City',
-          slug: 'battery-park-city',
+          id: 42,
+          name: 'Harlem',
+          slug: 'harlem',
           image: '/assets/images/neighborhoods/battery-park-city.jpg'
         },
         {
-          id: 32,
-          name: 'Tribeca',
-          slug: 'tribeca',
+          id: 43,
+          name: 'East Harlem',
+          slug: 'east-harlem',
           image: '/assets/images/neighborhoods/tribeca.jpg'
         },
         {
-          id: 33,
-          name: 'Financial District',
-          slug: 'financial-district',
+          id: 44,
+          name: 'South Harlem',
+          slug: 'south-harlem',
+          image: '/assets/images/financial-district.jpg'
+        },
+        {
+          id: 45,
+          name: 'Central Harlem',
+          slug: 'central-harlem',
+          image: '/assets/images/financial-district.jpg'
+        },
+        {
+          id: 46,
+          name: 'West Harlem',
+          slug: 'west-harlem',
+          image: '/assets/images/financial-district.jpg'
+        },
+        {
+          id: 47,
+          name: 'Manhattanville',
+          slug: 'manhattanville',
+          image: '/assets/images/financial-district.jpg'
+        },
+        {
+          id: 48,
+          name: 'Hamilton Heights',
+          slug: 'hamilton-heights',
+          image: '/assets/images/financial-district.jpg'
+        },
+        {
+          id: 49,
+          name: 'Washington Heights',
+          slug: 'washington-heights',
+          image: '/assets/images/financial-district.jpg'
+        },
+        {
+          id: 50,
+          name: 'Fort George',
+          slug: 'fort-george',
+          image: '/assets/images/financial-district.jpg'
+        },
+        {
+          id: 51,
+          name: 'Hudson Heights',
+          slug: 'hudson-heights',
+          image: '/assets/images/financial-district.jpg'
+        },
+        {
+          id: 52,
+          name: 'Inwood',
+          slug: 'inwood',
+          image: '/assets/images/financial-district.jpg'
+        },
+        {
+          id: 53,
+          name: 'Marble Hill',
+          slug: 'marble-hill',
           image: '/assets/images/financial-district.jpg'
         },
       ]
