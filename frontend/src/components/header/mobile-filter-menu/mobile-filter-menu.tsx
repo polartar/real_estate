46bb41e7cbc649770a083802c4d5d0a74164ab37
polartar <polaris.center@outlook.com>
2019-10-21 01:26:59 -0700

    better mobile search filter setup

--- frontend/src/components/header/mobile-filter-menu/mobile-filter-menu.tsx
+++ frontend/src/components/header/mobile-filter-menu/mobile-filter-menu.tsx
@@ -17,31 +17,27 @@ export class AppMenu {
   getMenu() {
     return (
       <ion-list mode="md">
-        <ion-item class="header">
-          Search Filters
-        </ion-item>
-
-        <ion-item button mode="md" detail detailIcon="arrow-dropright" onClick={() => this.display = 'locations'}>
+        <ion-item button mode="md" detail detailIcon="md-arrow-dropright" onClick={() => this.display = 'locations'}>
           Locations
         </ion-item>
 
-        <ion-item button mode="md" detail detailIcon="arrow-dropright" onClick={() => this.display = 'time'}>
+        <ion-item button mode="md" detail detailIcon="md-arrow-dropright" onClick={() => this.display = 'time'}>
           Time
         </ion-item>
 
-        <ion-item button mode="md" detail detailIcon="arrow-dropright" onClick={() => this.display = 'price'}>
+        <ion-item button mode="md" detail detailIcon="md-arrow-dropright" onClick={() => this.display = 'price'}>
           Price
         </ion-item>
 
-        <ion-item button mode="md" detail detailIcon="arrow-dropright" onClick={() => this.display = 'beds-baths'}>
+        <ion-item button mode="md" detail detailIcon="md-arrow-dropright" onClick={() => this.display = 'beds-baths'}>
           Bed and Baths
         </ion-item>
 
-        <ion-item button mode="md" detail detailIcon="arrow-dropright" onClick={() => this.display = 'building-type'}>
+        <ion-item button mode="md" detail detailIcon="md-arrow-dropright" onClick={() => this.display = 'building-type'}>
           Building Type
         </ion-item>
 
-        <ion-item aria-label="Search" class="search" button mode="md" detail detailIcon="arrow-dropright">
+        <ion-item aria-label="Search" class="search" button mode="md" detail detailIcon="md-arrow-dropright">
           Search
         </ion-item>
       </ion-list>
@@ -50,121 +46,107 @@ export class AppMenu {
 
   getTime() {
     return (
-      <div>
-        <ion-list>
-          <ion-item class="mobile-filter-menu-back">
-            <ion-button aria-label="Menu" class="reset" onClick={() => this.display = 'menu'}>
-              <ion-icon name="arrow-back" slot="icon-only" />
-            </ion-button>
-            Time
-          </ion-item>
-        </ion-list>
-
-        <div class="center-content">
-          <move-in-date-filter />
-        </div>
+      <div class="center-content">
+        <move-in-date-filter />
       </div>
     )
   }
 
   getPrice() {
     return (
-      <div>
-        <ion-list>
-          <ion-item class="mobile-filter-menu-back">
-            <ion-button aria-label="Menu" class="reset" onClick={() => this.display = 'menu'}>
-              <ion-icon name="arrow-back" slot="icon-only" />
-            </ion-button>
-            Price
-          </ion-item>
-        </ion-list>
-
-        <div class="center-content">
-          <price-filter />
-        </div>
+      <div class="center-content">
+        <price-filter />
       </div>
     )
   }
 
   getBuildingType() {
     return (
-      <div>
-        <ion-list>
-          <ion-item  class="mobile-filter-menu-back">
-            <ion-button aria-label="Menu" class="reset" onClick={() => this.display = 'menu'}>
-              <ion-icon name="arrow-back" slot="icon-only" />
-            </ion-button>
-            Building Type
-          </ion-item>
-        </ion-list>
-
-        <div class="center-content">
-          <building-type-filter />
-        </div>
+      <div class="center-content">
+        <building-type-filter />
       </div>
     )
   }
 
   getBedsBaths() {
     return (
-      <div>
-        <ion-list>
-          <ion-item class="mobile-filter-menu-back">
-            <ion-button aria-label="Menu" class="reset" onClick={() => this.display = 'menu'}>
-              <ion-icon name="arrow-back" slot="icon-only" />
-            </ion-button>
-            Beds and Baths
-          </ion-item>
-        </ion-list>
+      <div class="center-content">
+        <bedroom-filter />
 
-        <div class="center-content">
-          <bedroom-filter />
-
-          <bathroom-filter />
-        </div>
+        <bathroom-filter />
       </div>
     )
   }
 
+  getLocations() {
+    return [
+        <div class="center-content">
+          <location-filter />
+        </div>
+    ]
+  }
+
 
   render() {
-    let content;
+    let title, content;
 
     switch (this.display) {
-      case 'menu':
-        content = this.getMenu();
+      case 'locations':
+        title = 'Locations';
+        content = this.getLocations();
       break;
 
       case 'time':
+        title = 'Time';
         content = this.getTime();
       break;
 
       case 'price':
+        title = 'Price';
         content = this.getPrice();
       break;
 
       case 'building-type':
+        title = 'Building Type';
         content = this.getBuildingType();
       break;
 
       case 'beds-baths':
+        title = 'Beds and baths';
         content = this.getBedsBaths();
       break;
 
+      case 'menu':
       default:
+        title = 'Search Filters';
         content = this.getMenu();
       break;
     }
 
-    return (
-        <div class="mobile-filter-menu">
+    return [
+        <ion-header>
+          <ion-toolbar mode="md">
+            { this.display !== 'menu' ?
+            <ion-buttons slot="start">
+              <ion-button aria-label="Menu" class="reset" onClick={() => this.display = 'menu'}>
+                <ion-icon mode="md" name="md-arrow-back" slot="icon-only" />
+              </ion-button>
+            </ion-buttons>
+            : '' }
 
+            <ion-title>{title}</ion-title>
+
+            <ion-buttons slot="end">
+              <ion-button aria-label="Close" class="reset" onClick={() => this.closeModal()}>
+                <ion-icon mode="md" name="md-close" slot="icon-only" />
+              </ion-button>
+            </ion-buttons>
+          </ion-toolbar>
+        </ion-header>,
+
+        <ion-content class="mobile-filter-menu">
           {content}
-
-          <ion-button class="reset close" onClick={() => this.closeModal()}>
-            <ion-icon name="close" slot="icon-only" />
-          </ion-button>
-        </div>
-    )
+        </ion-content>
+    ]
   }
 }
