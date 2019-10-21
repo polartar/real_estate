f30555ef45fab0527d22b8d282cd35e6557801ed
polartar <polaris.center@outlook.com>
2019-10-21 13:23:12 -0700

    force md icons

--- frontend/src/components/header/filters/search-filters.tsx
+++ frontend/src/components/header/filters/search-filters.tsx
@@ -48,45 +48,45 @@ export class SearchFilters {
         <div class="section">
           <button aria-label="Location filter" class="button-reset dropdown" onClick={(e) => this.showFilterOptions(e, 'location-filter')}>
             Location
-            <ion-icon name="arrow-dropdown"></ion-icon>
+            <ion-icon mode="md" name="md-arrow-dropdown"></ion-icon>
           </button>
 
           <button aria-label="Move in date filter" class="button-reset dropdown" onClick={(e) => this.showFilterOptions(e, 'move-in-date-filter')}>
             Move In Date
-            <ion-icon name="arrow-dropdown"></ion-icon>
+            <ion-icon mode="md" name="md-arrow-dropdown"></ion-icon>
           </button>
 
           <button aria-label="Price filter" class="button-reset dropdown" onClick={(e) => this.showFilterOptions(e, 'price-filter')}>
             Price
-            <ion-icon name="arrow-dropdown"></ion-icon>
+            <ion-icon mode="md" name="md-arrow-dropdown"></ion-icon>
           </button>
 
           <button aria-label="Bedrooms filter" class="button-reset dropdown" onClick={(e) => this.showFilterOptions(e, 'bedroom-filter')}>
             Beds
-            <ion-icon name="arrow-dropdown"></ion-icon>
+            <ion-icon mode="md" name="md-arrow-dropdown"></ion-icon>
           </button>
 
           <button aria-label="Bathrooms filter" class="button-reset dropdown" onClick={(e) => this.showFilterOptions(e, 'bathroom-filter')}>
             Baths
-            <ion-icon name="arrow-dropdown"></ion-icon>
+            <ion-icon mode="md" name="md-arrow-dropdown"></ion-icon>
           </button>
 
           <button aria-label="Building types filter" class="button-reset dropdown" onClick={(e) => this.showFilterOptions(e, 'building-type-filter')}>
             Building Type
-            <ion-icon name="arrow-dropdown"></ion-icon>
+            <ion-icon mode="md" name="md-arrow-dropdown"></ion-icon>
           </button>
 
           <ion-button aria-label="Search" class="search">Search</ion-button>
 
           <ion-button aria-label="Search Filters" class="reset mobile-filters" onClick={() => this.launchMobileFilterMenu()}>
-            <ion-icon name="md-funnel" slot="start"></ion-icon>
+            <ion-icon mode="md" name="md-funnel" slot="start"></ion-icon>
             Filter
           </ion-button>
 
           <div class="spacer" />
 
           <button aria-label="Close Filters" class="button-reset close" onClick={() => this.toggleSearchFilterDisplay(false)}>
-            <ion-icon name="close" slot="icon-only"/>
+            <ion-icon mode="md" name="md-close" slot="icon-only"/>
           </button>
         </div>
       </div>
