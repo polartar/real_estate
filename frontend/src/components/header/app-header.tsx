a22759001e25a5b4c62810771c64b273a39c5350
polartar <polaris.center@outlook.com>
2019-10-22 14:25:29 -0700

    mobile search button launches straight into filters menu

--- frontend/src/components/header/app-header.tsx
+++ frontend/src/components/header/app-header.tsx
@@ -9,16 +9,21 @@ import { toggleSearchFilterDisplay } from "../../store/actions/search-filters";
 export class AppHeader {
   @Prop({ context: "store" }) store: Store;
   @State() displayFilter: boolean;
+  @State() size: string;
+  @State() isMobile: boolean;
   toggleSearchFilterDisplay: Action;
 
   componentDidLoad() {
     this.store.mapStateToProps(this, state => {
       const {
-        searchFilters: { displayFilter }
+        searchFilters: { displayFilter },
+        screenSize: { size, isMobile },
       } = state;
 
       return {
-        displayFilter
+        displayFilter,
+        size,
+        isMobile
       };
     });
 
@@ -37,6 +42,27 @@ export class AppHeader {
     return modal.present();
   }
 
+  async launchMobileFilterMenu() {
+    const modal = Object.assign(document.createElement('ion-modal'), {
+      component: 'mobile-filter-menu',
+      cssClass: 'app-menu'
+    });
+
+    document.body.appendChild(modal);
+    return modal.present();
+  }
+
+  activateSearch() {
+    console.log(this.isMobile);
+
+    if (this.isMobile) {
+      this.launchMobileFilterMenu();
+    }
+    else {
+      this.toggleSearchFilterDisplay(!this.displayFilter);
+    }
+  }
+
   render() {
     return [
       <header class="header-wrapper">
@@ -45,7 +71,7 @@ export class AppHeader {
             <img src="/assets/images/logo.svg" class="logo" alt="APT212 Logo"></img>
           </ion-router-link>
 
-          <button class="search" onClick={() => { this.toggleSearchFilterDisplay(!this.displayFilter) }}>
+          <button class="search" onClick={() => { this.activateSearch() }}>
             <svg class="feather feather-search" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><circle cx="11" cy="11" r="8" stroke="black" stroke-width="3" fill="transparent"></circle><line stroke="black" stroke-width="3" x1="24" x2="16.65" y1="24" y2="16.65"></line></svg>
             Search Apartments
           </button>
