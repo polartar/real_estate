4a5621ebe1e2e05401c2317cc5bed6eed0c6a8c8
polartar <polaris.center@outlook.com>
2020-04-15 15:40:00 -0700

    disable touch scroll on neighborhood map

--- frontend/src/components/neighborhood-map/neighborhood-map.tsx
+++ frontend/src/components/neighborhood-map/neighborhood-map.tsx
@@ -63,6 +63,9 @@ export class NeighborhoodMap {
           scrollZoom: false
         });
 
+        this.map.dragPan.disable();
+        this.map.touchZoomRotate.disable();
+
         this.map.addControl(new mapboxgl.NavigationControl());
 
         this.map.on('load', () => {
