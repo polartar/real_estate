fe8978e1f6c7fa380f52a2b8451ef9bb4ece31b0
polartar <polaris.center@outlook.com>
2020-06-25 11:36:23 -0700

    remove unused const on contact page

--- frontend/src/components/ask-question/ask-question.tsx
+++ frontend/src/components/ask-question/ask-question.tsx
@@ -8,41 +8,6 @@ export class AskQuestion {
 
   render() {
 
-    const people = [
-      {
-        name: 'Adi',
-        image: 'https://picsum.photos/200/200'
-      },
-      {
-        name: 'Margaret',
-        image: 'https://picsum.photos/200/200'
-      },
-      {
-        name: 'Milena',
-        image: 'https://picsum.photos/200/200'
-      },
-      {
-        name: 'Jeisohn',
-        image: 'https://picsum.photos/200/200'
-      },
-      {
-        name: 'Dustin',
-        image: 'https://picsum.photos/200/200'
-      },
-      {
-        name: 'Nina',
-        image: 'https://picsum.photos/200/200'
-      },
-      {
-        name: 'Rivka',
-        image: 'https://picsum.photos/200/200'
-      },
-      {
-        name: 'Dana',
-        image: 'https://picsum.photos/200/200'
-      }
-    ];
-
     return (
       <div class="ask-question-component">
         <div class="content-right">
