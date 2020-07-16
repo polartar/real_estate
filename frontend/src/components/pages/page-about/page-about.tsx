18d6c8623a8bc399f457558e5265cde7f9637e25
polartar <polaris.center@outlook.com>
2020-07-16 11:23:23 -0700

    add olark chat

--- frontend/src/components/pages/page-about/page-about.tsx
+++ frontend/src/components/pages/page-about/page-about.tsx
@@ -18,8 +18,8 @@ export class PageAbout {
         return [
             <app-header />,
             <ion-content class="page-about">
-    
-            
+
+
                 <div class="hero">
                     <lazy-image src="/assets/images/about/about-full.jpg" class="hero-bg" alt="background image" />
                     <section class="section">
@@ -53,7 +53,7 @@ export class PageAbout {
                         <lazy-image src="/assets/images/about/iphone.png" />
                     </div>
 
-                  
+
                 </div>
 
                 <hr />
@@ -96,7 +96,7 @@ export class PageAbout {
                 </div>
 
                 <div class="people-wrapper">
-                    
+
                     <div class="people">
                         <div class="person">
                             <lazy-image src="/assets/images/about/adrian.jpg" />
@@ -144,11 +144,11 @@ export class PageAbout {
                 </div>
 
                     <div class="edge-wrapper">
-                        <h2>Our Edge</h2> 
+                        <h2>Our Edge</h2>
                         <div class="edge">
-                            
+
                             <div class="row">
-                                   
+
                                 <lazy-image src="/assets/images/about/flexibility.png" />
                                 <h3>Flexible Lease Terms</h3>
                                 <p>
@@ -162,7 +162,7 @@ export class PageAbout {
                                         No need to commit to a long-term lease. APT212 offers 30-day minimum lease term for furnished units - a rare find in New York City.
                                     </p>
                             </div>
-                                
+
                         </div>
                         <div class="edge">
                             <div class="row">
@@ -179,7 +179,7 @@ export class PageAbout {
                                     No need to commit to a long-term lease. APT212 offers 30-day minimum lease term for furnished units - a rare find in New York City.
                                 </p>
                             </div>
-                                
+
                         </div>
                         <div class="edge">
                             <div class="row">
@@ -196,18 +196,14 @@ export class PageAbout {
                                     No need to commit to a long-term lease. APT212 offers 30-day minimum lease term for furnished units - a rare find in New York City.
                                 </p>
                             </div>
-                                
+
                         </div>
                     </div>
-                
+
             </section>
             <app-footer />
-    
-            <ion-fab horizontal="end" vertical="bottom" slot="fixed">
-                <ion-fab-button class="chat">
-                <svg class="chat-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="#ffffff" d="M416 192c0-88.4-93.1-160-208-160S0 103.6 0 192c0 34.3 14.1 65.9 38 92-13.4 30.2-35.5 54.2-35.8 54.5-2.2 2.3-2.8 5.7-1.5 8.7S4.8 352 8 352c36.6 0 66.9-12.3 88.7-25 32.2 15.7 70.3 25 111.3 25 114.9 0 208-71.6 208-160zm122 220c23.9-26 38-57.7 38-92 0-66.9-53.5-124.2-129.3-148.1.9 6.6 1.3 13.3 1.3 20.1 0 105.9-107.7 192-240 192-10.8 0-21.3-.8-31.7-1.9C207.8 439.6 281.8 480 368 480c41 0 79.1-9.2 111.3-25 21.8 12.7 52.1 25 88.7 25 3.2 0 6.1-1.9 7.3-4.8 1.3-2.9.7-6.3-1.5-8.7-.3-.3-22.4-24.2-35.8-54.5z" /></svg>
-                </ion-fab-button>
-            </ion-fab>
+
+
             </ion-content>
         ];
     }
