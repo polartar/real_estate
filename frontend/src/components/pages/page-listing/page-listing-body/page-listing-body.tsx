8a82fc5d397b2523e87b8efb3937226b3d29bede
polartar <polaris.center@outlook.com>
2020-07-16 10:58:34 -0700

    fix neighborhood page paths for seo

--- frontend/src/components/pages/page-listing/page-listing-body/page-listing-body.tsx
+++ frontend/src/components/pages/page-listing/page-listing-body/page-listing-body.tsx
@@ -384,7 +384,7 @@ export class PageListingBody {
                     <div class="description" innerHTML={n.description}>
 
 
-                      <a href={`/nyc-neighborhood/${n.slug}`} class="button-dark">
+                      <a href={`/nyc-neighborhood/${n.slug}/apartments`} class="button-dark">
                         Explore
                       </a>
                     </div>
