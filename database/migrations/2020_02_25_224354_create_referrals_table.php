ba08002f36da26e55ccec698bbd53bae19cadff0
polartar <polaris.center@outlook.com>
2020-09-10 05:40:26 +1000

    implement signup function

--- database/migrations/2020_02_25_224354_create_referrals_table.php
+++ database/migrations/2020_02_25_224354_create_referrals_table.php
@@ -15,11 +15,11 @@ class CreateReferralsTable extends Migration
     {
         Schema::create('referrals', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->integer('referrer_id');
+            $table->integer('referrer_uid');
             $table->string('referral_name');
             $table->string('referral_email');
             $table->string('referral_phone')->nullable();
-            $table->string('referer_agent')->nullable();
+            $table->string('referrer_agent')->nullable();
             $table->text('referral_details')->nullable();
             $table->timestamps();
         });
