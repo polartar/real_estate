cf1f48400ddde777885badfdd5b737e3e00ee673
polartar <polaris.center@outlook.com>
2020-09-10 14:28:34 -0700

    two click for home hero search thingy

--- frontend/src/components/pages/page-home/page-home.tsx
+++ frontend/src/components/pages/page-home/page-home.tsx
@@ -98,8 +98,22 @@ export class PageHome {
 
     if (market == "furnished") {
       this.searchLink = "/search-apartments";
-    } else {
+      document.getElementById('furnished').style.cssText = 'background-color: #f5f5f7; color: #131414;';
+      document.getElementById('sales').style.cssText = 'background-color: #131414; color: #f5f5f7;';
+      document.getElementById('rentals').style.cssText = 'background-color: #131414; color: #f5f5f7;';
+      document.getElementById('home-search').style.cssText = 'background-color: #f5f5f7;';
+    } else if (market == "sales") {
       this.searchLink = "/coming-soon";
+      document.getElementById('sales').style.cssText = 'background-color: #f5f5f7; color: #131414;';
+      document.getElementById('rentals').style.cssText = 'background-color: #131414; color: #f5f5f7;';
+      document.getElementById('furnished').style.cssText = 'background-color: #131414; color: #f5f5f7;';
+      document.getElementById('home-search').style.cssText = 'background-color: #f5f5f7;';
+    } else if (market == "rentals") {
+      this.searchLink = "/coming-soon";
+      document.getElementById('rentals').style.cssText = 'background-color: #f5f5f7; color: #131414;';
+      document.getElementById('furnished').style.cssText = 'background-color: #131414; color: #f5f5f7;';
+      document.getElementById('sales').style.cssText = 'background-color: #131414; color: #f5f5f7;';
+      document.getElementById('home-search').style.cssText = 'background-color: #f5f5f7;';
     }
   }
 
@@ -138,14 +152,14 @@ export class PageHome {
               </div>
 
                 <div class="hero-child info">
-                    <button class="button-reset market-tab" onClick={() =>  this.setSearchLink("furnished") }>{ furnishedText }</button>
+                    <button id="furnished" class="button-reset market-tab" onClick={() =>  this.setSearchLink("furnished") }>{ furnishedText }</button>
 
-                    <button class="button-reset market-tab" onClick={() => window.location.replace( salesLink )}>Rentals</button>
+                    <button id="rentals" class="button-reset market-tab" onClick={() => this.setSearchLink("rentals") }>Rentals</button>
 
-                    <button class="button-reset market-tab" onClick={() => window.location.replace( rentalsLink )}>Sales</button>
+                    <button id="sales" class="button-reset market-tab" onClick={() => this.setSearchLink("sales") }>Sales</button>
 
                     <div class="home-search-wrapper">
-                      <input type="text" class="home-search" onClick={() => window.location.replace( this.searchLink )}/>
+                      <input id="home-search" type="text" class="home-search" onClick={() => window.location.replace( this.searchLink )}/>
                       <button class="button-reset search-icon">
                         <svg class="feather feather-search" viewBox="0 0 25 24" version="1.1"><g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><path d="M24,21.1886008 L18.6803754,15.9186997 C19.949079,14.3099652 20.6588954,12.3179013 20.6588954,10.238277 C20.6588954,5.14393658 16.472729,1 11.3303363,1 C6.18621089,1 2,5.14393658 2,10.238277 C2,15.3308573 6.18616646,19.4765539 11.3303363,19.4765539 C13.3071236,19.4765539 15.2318387,18.8457674 16.8196842,17.7010588 L22.1704099,23 L24,21.1886008 Z M11.3302919,16.9140717 C7.61273046,16.9140717 4.58934605,13.9182757 4.58934605,10.238365 C4.58934605,6.55849823 7.61268603,3.56265825 11.3302919,3.56265825 C15.0461205,3.56265825 18.0694605,6.55845423 18.0694605,10.238365 C18.0694605,12.2063608 17.1982293,14.0643123 15.6796059,15.3379854 C14.4664401,16.3537734 12.9218251,16.9140717 11.3302919,16.9140717 Z" fill="#f5f5f7"></path></g></svg>
                       </button>
